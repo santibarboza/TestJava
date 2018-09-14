fca5f0695c50457629cb4fd1016a1122cbe06c69
Santi <santii.barboza@gmail.com>
2018-09-14 11:16:25 -0300

    test presenter

--- src/main/java/hello/OCRestController.java
+++ src/main/java/hello/OCRestController.java
@@ -65,7 +65,7 @@ public class OCRestController {
         presenter.updatePCView("PCViejo");
         presenter.updatePCView("PCNuevo");
         presenter.updateLogs("LogNuevo");
-        return view.obtenerAcciones();
+        return view.obtenerAcciones().add(new AccionCambio("setID",id));;
     }
 
 
