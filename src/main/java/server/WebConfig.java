cb1844e38b7010d4b4d67d641248a719be602f7c
Federico Paganetto <federico.paganetto@hotmail.com>
2018-10-08 18:41:22 -0300

    paga se recibe

--- src/main/java/server/WebConfig.java
+++ src/main/java/server/WebConfig.java
@@ -7,6 +7,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
 @EnableWebMvc
